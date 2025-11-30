fabc4468328ae869ea856e7cd4319244a2a1b69b
Leis Loggers <leis_loggers0e@icloud.com>
2025-11-30 23:17:09 +0300

    Готовимся сплитить файлы

---
+++ functions/splitter.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+// Функция для деления файла на части по 80 МБ
+async function splitFile(inputFilePath, outputDir) {
+    return new Promise((resolve, reject) => {
+        
+    })
+}
